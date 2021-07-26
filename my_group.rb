cd8ae449e421c2869ff59e77f0bdd0d7e707682f
maxarvid <max.arvid.anderson@gmail.com>
2021-07-26 19:29:33 +0200

    add print loop for my_group.rb file

--- my_group.rb
+++ my_group.rb
@@ -5,3 +5,7 @@ person_2 = {name: "Clint", gender: true, age: 50}
 person_3 = {name: "Bob", gender: "uncle", age: -1}
 
 my_group = [person_1, person_2, person_3]
+
+my_group.each do |person|
+    puts "#{person[:name].capitalize()} is a #{person[:age]} year old #{person[:gender]}"
+end
