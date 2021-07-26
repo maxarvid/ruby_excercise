7d58ba85558b96bd6f632c013ad9ad44d373207e
maxarvid <max.arvid.anderson@gmail.com>
2021-07-26 19:13:36 +0200

    add my_group array of hashes for persons 1-3

---
+++ my_group.rb
@@ -0,0 +1,7 @@
+my_group = Array.new
+
+person_1 = {name: "Joe", gender: "nb", age: 60}
+person_2 = {name: "Clint", gender: true, age: 50}
+person_3 = {name: "Bob", gender: "uncle", age: -1}
+
+my_group = [person_1, person_2, person_3]
