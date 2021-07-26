3a8520a1378ec38dcf514220e1227b10de74b21a
maxarvid <max.arvid.anderson@gmail.com>
2021-07-26 19:55:49 +0200

    add ruby operators

---
+++ ruby_operators.rb
@@ -0,0 +1,39 @@
+a = 20
+b = 10
+
+# Arithmetic operators
+a + b
+a - b
+a * b
+a / b
+a % b
+a**b
+
+# Comparison operators
+a == b
+a != b
+a > b
+a < b 
+a >= b 
+a <= b 
+a <=> b #combined comparison operator
+a === b 
+a.eql? b 
+a.equal? b 
+
+# Assignment operators
+c = a + b 
+c += a 
+c -= a 
+c *= a 
+c /= a 
+c %= a 
+c **= a 
+
+# Logical operators
+true && true
+false || true
+!false
+true and true
+true or false
+not false
