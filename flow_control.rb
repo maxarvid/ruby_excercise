ed0deb14df2b5e04a3c51d7e9b66a8095d31b350
maxarvid <max.arvid.anderson@gmail.com>
2021-07-27 15:01:23 +0200

    Add Ruby flow control question 2 with if, elsif, else solution

--- flow_control.rb
+++ flow_control.rb
@@ -3,3 +3,15 @@
 str = 'this is my string'
 
 puts str.upcase
+
+# 2 take number from user, return if it's btwn 0 and 50 or 51-100
+
+puts "Please enter a number betweeen 0-100"
+num = gets.to_i
+if num >= 0 && num < 51
+    puts "The number is between 0 and 50."
+elsif num > 50 && num <= 100
+    puts "The number is between 51 and 100"
+else 
+    puts "Out of range"
+end
